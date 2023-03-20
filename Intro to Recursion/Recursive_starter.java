95f4c1d14dcea4876cb7388af0295e31aaf515b3
Katie Wang <kwang3@stu.naperville203.org>
2023-03-20 14:13:51 -0500

    a

--- Intro to Recursion/Recursive_starter.java
+++ Intro to Recursion/Recursive_starter.java
@@ -8,54 +8,64 @@ public class Recursive_starter
 {
 
     public static void main(String[] args)
-	{
+    {
 
-		foo(5);
-		//System.out.print(sum (4));
+        //foo(5);
+        System.out.print(sum (4));
 
-		//System.out.println(recursiveMult(6,5)  );
+        //System.out.println(recursiveMult(6,5)  );
 
-		//System.out.println("exp = abc");
-		//guess("abcdefg",0);
+        //System.out.println("exp = abc");
+        //guess("abcdefg",0);
 
-		//reverse_print("abcde");
+        //reverse_print("abcde");
 
-		//int_guess(1);
+        //int_guess(1);
 
-		//System.out.println("The sum is " + sum (4));
+        //System.out.println("The sum is " + sum (4));
 
-		//System.out.println( btp (3,3));
-
-
+        //System.out.println( btp (3,3));
 
 
 
-	}//end of main
+
+
+    }//end of main
 
 
 public static int btp(int a, int b)
 {
 
 
-	if (b == 0)
-		return 1;
-	else
-		return a * btp(a, b - 1);
+    if (b == 0)
+        return 1;
+    else
+        return a * btp(a, b - 1);
 }
 
 public static int foo(int x)
 {
-	if (x > 0)
-		foo(x+1);
-
-	return 0;
+    if (x > 0){
+        System.out.println(x);
+        foo(x+1);
+        
+    }
+    return 0;
 }
+
+public static int sum(int s){
+    
+    
+    return s + sum(s);
+    
+}
+
 
 public static void reverse_print(String str)
 {
 
 
-	reverse_print("blah");
+    reverse_print("blah");
 
 
 
@@ -71,15 +81,15 @@ public static void reverse_print(String str)
 
 public static int mystery (int num)
 {
-	//System.out.println(num );
-	if (num == 1)
-		return 1;
-	else
-	{
+    //System.out.println(num );
+    if (num == 1)
+        return 1;
+    else
+    {
 
-		return num + mystery(num-1);
+        return num + mystery(num-1);
 
-	}
+    }
 
 
 }
