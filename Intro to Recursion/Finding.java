73469009cda50d7bcc482b014aab2b981b9af68f
Katie Wang <kwang3@stu.naperville203.org>
2023-04-03 14:14:17 -0500

    a

---
+++ Intro to Recursion/Finding.java
@@ -0,0 +1,23 @@
+
+/**
+ * Write a description of class Finding here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Finding
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class Finding
+     */
+    public static void main(String[] args){
+                
+            
+            
+    }
+
+    
+}
