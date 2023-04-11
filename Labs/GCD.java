be0d4d26e5c2216353c64832eee3ee4329f3b33c
Katie Wang <kwang3@stu.naperville203.org>
2023-04-11 14:18:37 -0500

    a

---
+++ Labs/GCD.java
@@ -0,0 +1,37 @@
+
+/**
+ * Write a description of class GCD here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class GCD
+{
+    // instance variables - replace the example below with your own
+    
+    /**
+     * Constructor for objects of class GCD
+     * 
+     */
+   
+    
+    public static void main(String[] args)
+    {
+        // initialise instance variables
+        System.out.println(gcd(25,1075));
+        
+    }
+    
+    public static int gcd(int num1, int num2){
+        
+        if (num2 <= num1 && num1%num2 == 0){
+            return num2;
+        }else if (num1 < num2){
+            return gcd(num2, num1);
+        }else{
+            return gcd(num2, num1%num2);
+        }
+        
+    }
+
+}
---
+++ Labs/getRich.java
@@ -0,0 +1,27 @@
+
+/**
+ * Write a description of class getRich here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class getRich
+{
+    // instance variables - replace the example below with your own
+    
+
+    /**
+     * Constructor for objects of class getRich
+     */
+    public static void main(String[] arg){
+        System.out.println();
+        
+        
+    
+    }
+    public static int getRich(int money)
+    {
+        
+        return;
+    }
+}
