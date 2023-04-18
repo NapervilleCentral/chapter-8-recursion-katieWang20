590a4f2ada45e2202d94ea2c3ecb389f9f531576
Katie Wang <kwang3@stu.naperville203.org>
2023-04-18 14:20:30 -0500

    fgh

--- Labs/PinWordEnumerator.java
+++ Labs/PinWordEnumerator.java
@@ -33,61 +33,89 @@ public class PinWordEnumerator
         String[] map = {"0","1","ABC","DEF","GHI","JKL","MNO","PQRS","TUV","WXYZ"};
          String word = "";
          
-        for (int i = 1; i <= n.length(); i ++){
-            word += map[Integer.valueOf(n.substring(i - 1, i))].substring(0, 1);
-        }
+        //for (int i = 1; i <= n.length(); i ++){
+            
+           // word += map[Integer.valueOf(n.substring(i - 1, i))].substring(0, 1);
+        //}
          
-        System.out.println("start word: " + word);
+           
+            for (int i = 0; i < n.length(); i ++){
+                 int singleNum = Integer.valueOf(n.substring(i-1, i));
+                 for (int j = 0; j < map[singleNum].length(); j ++){
+                     System.out.println(map[singleNum].substring(j, j+1));
+                    
+                 }
+                   
+                //enumerateWords(n, "");
+                //newWord += map[Integer.valueOf(pin.substring(i - 1, i))].substring(i, i+1);
+                
+            //}
          
+        //System.out.println("word: " + word);
+         //add other letters to a, then add other letters to the second, then third
          
+         // empty string
          
-         enumerateWords(n, word);
         
 
+        }
     }
-
     /**
            Recursively generates all possible 'words' that represent
            the telephone keypad encoding of a number.
            @param pin the number
            @param word representing the encoding a the pin
+           
          */
         public static void enumerateWords(String pin, String word)
         {
-            System.out.println(word);
-            String newWord = "";
-            String[] map = {"0","1","ABC","DEF","GHI","JKL","MNO","PQRS","TUV","WXYZ"};                
+            //System.out.println(word);
+            String newWord = word;
+            //               0   1   2      3     4     5     6    7      8     9 
+            String[] map = {"0","1","ABC","DEF","GHI","JKL","MNO","PQRS","TUV","WXYZ"};         
+            
+            
+           
+            enumerateWords(pin, newWord);
+            
+            
+            /*
+             * 
+             * garbage disaster 
             for (int i = 0; i < pin.length(); i++){
-                int singleNum = Integer.valueOf(pin.substring(i, i+1));
-                 
-                
-                    
-                    for (int j = 0; j < map.length; j ++){
-                        for (int a = 0; a < map[j].length(); a ++){
-                              if (map[j].substring(a, a+1).equals(word.substring(i, i+1))){
-                                if (i != 3 && i != 4){
-                                    newWord += map[singleNum].substring(i, i+1);
-                                }
-                            }  
+                int singlePinNum = Integer.valueOf(pin.substring(i, i+1));
+                for (int mapIndex = 0; mapIndex < map.length; mapIndex ++){
+                    for (int mapLetterIndex = 0; mapLetterIndex < map[mapIndex].length(); mapLetterIndex ++){
+                        if (map[singlePinNum].substring(mapLetterIndex, mapLetterIndex + 1).equals(word.substring(i, i+1))){
+                            if (singlePinNum == 1){
+                                newWord += "1";
+                            }else if (singlePinNum == 0){
+                                newWord += "0";
+                            }else{
+                                newWord  
+                               
+                            }
                         }
                     }
-                    
-
-                                     
+                }
+                
+                
+                
+                */    
             
             }
             
             
             
-            enumerateWords(pin, newWord);
-            
-            
-        }
-        
-        
-        
-        
     }
+            
+            
+        
+        
+        
+        
+        
+    
 
 
 
