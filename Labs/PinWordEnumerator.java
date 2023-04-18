0944ac7d67b8be5b3819a1344c2be0db35245dfa
Katie Wang <kwang3@stu.naperville203.org>
2023-04-18 10:09:13 -0500

    fgh

--- Labs/PinWordEnumerator.java
+++ Labs/PinWordEnumerator.java
@@ -21,6 +21,7 @@ public class PinWordEnumerator
        phone keypad encodings for a number.
        @param n a string representing the number
      */
+    
     public static void enumerateWords(String n)
     {
          // Implement a recursive method that generates
@@ -31,32 +32,14 @@ public class PinWordEnumerator
          //System.out.println(n);
         String[] map = {"0","1","ABC","DEF","GHI","JKL","MNO","PQRS","TUV","WXYZ"};
          String word = "";
-         for (int i = 0; i < n.length(); i ++){
-             if (n.charAt(i) == '0'){
-                word += "0";    
-            }else if (n.charAt(i) == '1'){
-                word += "1";
-            }else if  (n.charAt(i) == '2'){
-                word += "A";
-            }else if (n.charAt(i) == '3'){
-                word += "D";
-            }else if  (n.charAt(i) == '4'){
-                word += "G";
-            }else if (n.charAt(i) == '5'){
-                word +="J";
-            }else if (n.charAt(i) == '6'){
-                word += "M";
-            }else if (n.charAt(i) == '7'){
-                word += "P";
-            }else if (n.charAt(i) == '8'){
-                word += "T";
-            }else if (n.charAt(i) == '9'){
-                word += "W";
-            }
-            
-         }
-        
-         // just make the word... 
+         
+        for (int i = 1; i <= n.length(); i ++){
+            word += map[Integer.valueOf(n.substring(i - 1, i))].substring(0, 1);
+        }
+         
+        System.out.println("start word: " + word);
+         
+         
          
          enumerateWords(n, word);
         
@@ -71,55 +54,40 @@ public class PinWordEnumerator
          */
         public static void enumerateWords(String pin, String word)
         {
+            System.out.println(word);
             String newWord = "";
-            /**
-             * absolute garbage disaster 
-            if (pin.length() == 0)
-            {
-                System.out.printf("%s%n",word);
-            }
-            else
-            {
-                String[] map = {"0","1","ABC","DEF","GHI","JKL","MNO","PQRS","TUV","WXYZ"};
+            String[] map = {"0","1","ABC","DEF","GHI","JKL","MNO","PQRS","TUV","WXYZ"};                
+            for (int i = 0; i < pin.length(); i++){
+                int singleNum = Integer.valueOf(pin.substring(i, i+1));
+                 
                 
-                for (int i =0; i < word.length();i ++){
-                    if (word.charAt(i) == '1'){
-                        newWord += "1";
-                    }else if (word.charAt(i) == '0'){
-                        newWord += "0";
-                    }else{
-                        for (int a = 0; a < map.length; a ++){
-                            for (int j = 0; j < map[a].length(); j++){
-                                if (word.charAt(i) == map[a].charAt(j)){
-                                    if (j == 2){
-                                        newWord+= map[a].charAt(j-1);
-                                    
-                                    
-                                    }else if (j == 0){
-                                        newWord += map[a].charAt(j+1);
-                                    }else if (j == 4){
-                                        newWord += map[a].charAt(j - 1);
-                                    
-                                    }
-
-                            }
+                    
+                    for (int j = 0; j < map.length; j ++){
+                        for (int a = 0; a < map[j].length(); a ++){
+                              if (map[j].substring(a, a+1).equals(word.substring(i, i+1))){
+                                if (i != 3 && i != 4){
+                                    newWord += map[singleNum].substring(i, i+1);
+                                }
+                            }  
                         }
                     }
-                
-                }
-                
-                
-                
-                
-                
+                    
+
+                                     
+            
             }
             
-            enumerateWords(pin, word);
+            
+            
+            enumerateWords(pin, newWord);
+            
             
         }
         
-        */
+        
+        
+        
     }
 
 
-}
+
